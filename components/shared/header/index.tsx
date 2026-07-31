c7f5a4b39d601457fec111fb5dc4e71e8ec58a3f
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-07-31 16:54:15 +0300

    chore: add logo SVG , update header component to use logo variable , udpate global css to match v4 tailwind css constraction and udpate usestate+useEffect to useSyncExternalStore

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,8 +4,9 @@ import { APP_NAME } from '@/lib/constants';
 import Link from 'next/link';
 import Image from 'next/image';
 import ModeToggle from './mode-toggel';
+// import {logo} from '@/public/images/logo.svg';
 
-
+const logo = "/images/logo.svg";
 
 const Header = () => {
     return (
@@ -14,7 +15,7 @@ const Header = () => {
 
                 <div className="flex-start">
                 <Link href="/" className="flex-start">
-                    <Image src="/logo.svg" alt={`${APP_NAME} logo`}  width={48} height={48} priority />
+                    <Image src={logo} alt={`${APP_NAME} logo`}  width={48} height={48} priority />
                     <span className="hidden lg:block font-bold text-2xl ml-3"> {APP_NAME}</span>
                 </Link>
                 </div>
--- components/shared/header/mode-toggel.tsx
+++ components/shared/header/mode-toggel.tsx
@@ -1,37 +1,51 @@
+ 
 'use client';
-import { useState, useEffect } from "react";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
+import { useState, useEffect, useSyncExternalStore } from "react";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuLabel, DropdownMenuGroup } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon, SunMoon } from "lucide-react";
 
+function useMounted() {
+    return useSyncExternalStore(
+        () => () => { }, // Empty subscribe function (we don't need to subscribe to anything)
+        () => true,     // getSnapshot (returns true on client)
+        () => false     // getServerSnapshot (returns false on server)
+    );
+}
+
 const ModeToggle = () => {
-    const [mounted, setMounted] = useState(false);
+    // const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
 
+    const mounted = useMounted();
+    if (!mounted) return null;
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
 
-    if (!mounted) {
-        return null;
-    }
+    // useEffect(() => {
+    //     setMounted(true);
+    // }, []);
+
+    // if (!mounted) {
+    //     return null;
+    // }
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger render={ 
+            <DropdownMenuTrigger render={
                 <Button variant='ghost' className="focus-visible:ring-0 focus-visible:ring-offset-0">
                     {theme === "system" ? <SunMoon /> : theme === "dark" ? <MoonIcon /> : <SunIcon />}
                 </Button>
             }>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-32">
+                <DropdownMenuGroup>
                 <DropdownMenuLabel>Appearance</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <Button variant="ghost" onClick={() => setTheme("light")}>Light</Button>
                 <Button variant="ghost" onClick={() => setTheme("dark")}>Dark</Button>
                 <Button variant="ghost" onClick={() => setTheme("system")}>System</Button>
+                </DropdownMenuGroup>
             </DropdownMenuContent>
         </DropdownMenu>
     );
