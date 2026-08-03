6adb8d19b44d36c13a6dfc2f4decb9f1994fe651
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-03 18:16:35 +0300

    feat: add product listing component and sample data; update homepage to display featured products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  return <>Prostore</>
+
+  return <>
+  <ProductList data={sampleData.products} title="Featured Products" />
+  </>
 }
  
 export default HomePage ;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const ProductList = ({ data, title , limit=4 }: { data: any; title?: string; limit?: number }) => {
+
+    const limitedData = limit ? data.slice(0, limit) : data; // Limit to 8 products
+    return (
+        <div className="my-10">
+            <h2 className="h2-bold mb-4">{title}</h2>
+            {data.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {limitedData.map((product: any) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <div>{product.name}</div>
+                    ))}
+                </div>
+
+            ) : (
+                <div>
+                    <p>No products found</p>
+                </div >
+            )}
+        </div >
+    );
+}
+
+export default ProductList;
