6439d867ee8d236e275d5e26c5a4b87be48d4c44
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-04 00:05:37 +0300

    chore: update getLatestProducts to use configurable LATEST_PRODUCTS_LIMIT

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,6 +2,7 @@
 
 import { PrismaClient } from "../generated/prisma/client";
 import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 
@@ -9,7 +10,7 @@ export async function getLatestProducts() {
   const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
-    take: 4,
+    take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
       createdAt: "desc",
     },
