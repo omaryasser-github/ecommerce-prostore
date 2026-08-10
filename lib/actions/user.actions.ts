a553518b79b4cf28cd61ce9cb7e07fc56a0ae1b4
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-11 00:17:06 +0300

    feat: enhance error handling in sign-up process and format error messages

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import {signIn , signOut} from "@/auth"
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import {hashSync} from "bcrypt-ts-edge";
 import {prisma} from "@/db/prisma";
+import {formatError} from "../utils";
 
 
 export async function signInWithCredentials(prevState:unknown, formData: FormData) {
@@ -57,6 +58,6 @@ export async function signUpUser(prevState:unknown, formData: FormData) {
         if(isRedirectError(error)) {
             throw error;
         }
-        return { success: false , message: 'User was not registered' };
+        return { success: false , message: formatError(error) };
     }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 // convert a prisma object to a regular js object
 export function convertToPlainObject<T>(value: T): T {
@@ -13,6 +12,32 @@ export function convertToPlainObject<T>(value: T): T {
 
 // Format number with decemal places a
 export function formatNumberWithDecimal(num: number): string {
-  const [int, decimal] = num.toString().split('.');
-  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+}
+
+// format errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+  if (error.name === "ZodError") {
+    // Handle Zod validation errors
+    const fieldErrors = Object.keys(error.errors).map((field) => ({
+      message: error.errors[field].message,
+    }));
+
+    return fieldErrors.join(", ");
+  } else if (
+    error.name === "PrismaClientValidationError" &&
+    error.code === "P2002"
+  ) {
+    //  Handle prisma error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return ` ${field.charAt(0).toUpperCase() + field.slice(1)} alraedy exists`;
+  } else {
+    // handle other errors
+
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error);
+  }
 }
