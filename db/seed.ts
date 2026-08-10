915bd70aa50191bdbaef67e1215cba3e96e04061
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-10 16:37:15 +0300

    feat: update sample data and seed script for user creation , hash user passwords using bcrypt-ts-edge

--- db/seed.ts
+++ db/seed.ts
@@ -1,15 +1,21 @@
-import {config} from "dotenv";
-config({path: ".env"});
+import { config } from "dotenv";
+config({ path: ".env" });
 
 // Correct import for custom output
-import { PrismaClient } from '../lib/generated/prisma/client';     
+import { PrismaClient } from "../lib/generated/prisma/client";
 import sampleData from "./sample-data";
 
 async function main() {
   const prisma = new PrismaClient();
   //  delete every thing in the product table and then create new products from the sample data
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
