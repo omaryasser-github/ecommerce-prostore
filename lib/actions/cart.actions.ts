c0388922f3da1ece2a62140b213618be01957577
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-13 22:30:58 +0300

    feat: implement addToCart and getMyCart functions with session handling and error management

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,10 +1,73 @@
-'use server'
+"use server";
 
 import { CartItem } from "@/types";
+import { cookies } from "next/headers";
+import { convertToPlainObject, formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addToCart(data: CartItem) {
-    return {
-        success: true,
-        message: "Item added to cart successfully",
+  try {
+    // check  for cart cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+
+    if (!sessionCartId) {
+      throw new Error("Session cart id not found");
     }
+
+    // get session and user id
+    const session = await auth();
+
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // Get cart 
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
+    return {
+      success: true,
+      message: "Item added to cart successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+
+  if (!sessionCartId) {
+    throw new Error("Session cart id not found");
+  }
+
+  // get session and user id
+  const session = await auth();
+
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database using userId and sessionCartId
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
