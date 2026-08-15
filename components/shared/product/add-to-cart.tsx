5c7eabc33a7809b2baaaf97df35b9d1676df1c22
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-08-15 12:31:10 +0300

    feat: enhance AddToCart component with loading state and transition handling

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -4,33 +4,39 @@
 import { CartItem, Cart } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, ShoppingCart, Minus } from "lucide-react";
+import { Plus, ShoppingCart, Minus, Loader } from "lucide-react";
 import { toast } from "sonner";
 import { ToastAction } from "@/components/ui/toast";
 import { addToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
-import { startTransition } from "react";
+import { useTransition } from "react";
 
 const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
 
        const router = useRouter();
        // const {toast} = useToast();
 
+       const [isPending, startTransition] = useTransition();
+
        const handleAddToCart = async () => {
-              const res = await addToCart(item);
 
-              if (!res!.success) {
-                     toast.error("Item not created", {
-                            description: "Your changes have not been saved.",
-                     })
-              }
+              startTransition(async () => {
 
-              toast.success("Item created", {
-                     description: res!.message,
-                     action: (
-                            <ToastAction className='bg-primary text-white hover:bg-gray-800' onClick={() => router.push("/cart")}>
-                                   Go View Cart
-                            </ToastAction>
-                     ),
+                     const res = await addToCart(item);
+
+                     if (!res!.success) {
+                            toast.error("Item not created", {
+                                   description: "Your changes have not been saved.",
+                            })
+                     }
+
+                     toast.success("Item created", {
+                            description: res!.message,
+                            action: (
+                                   <ToastAction className='bg-primary text-white hover:bg-gray-800' onClick={() => router.push("/cart")}>
+                                          Go View Cart
+                                   </ToastAction>
+                            ),
+                     });
               });
        };
 
@@ -55,16 +61,16 @@ const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
        return existItem ? (
               <div>
                      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-                            <Minus className='w-4 h-4' />
+                            {isPending ? <Loader className='w-4 h-4 animate-spin' /> : <Minus className='w-4 h-4' />}
                      </Button>
                      <span className='px-2'>{existItem.qty}</span>
                      <Button type='button' variant='outline' onClick={handleAddToCart}>
-                            <Plus className='w-4 h-4' />
+                            {isPending ? <Loader className='w-4 h-4 animate-spin' /> : <Plus className='w-4 h-4' />}
                      </Button>
               </div>
        ) : (
               <Button className='w-full' type='button' onClick={handleAddToCart}>
-                     <Plus className='w-4 h-4' />
+                     {isPending ? <Loader className='w-4 h-4 animate-spin' /> : <Plus className='w-4 h-4' />}
                      Add To Cart
               </Button>
        );
